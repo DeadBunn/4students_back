1568cfe88ac613fe0acdcd8ba340c48b5a053f42
Dmitriy Mikhaylov <greatbox04@mail.ru>
2023-11-04 16:59:16 +0300

    bugfix: Обновил политику CORS

--- src/main/kotlin/ru/students/plugins/HTTP.kt
+++ src/main/kotlin/ru/students/plugins/HTTP.kt
@@ -6,11 +6,17 @@ import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
     install(CORS) {
+        allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowHeader(HttpHeaders.Authorization)
+        exposeHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.AccessControlAllowHeaders)
+        exposeHeader(HttpHeaders.AccessControlAllowHeaders)
+        allowCredentials = true
         anyHost()
+        println("Cors has been configured")
     }
 }
