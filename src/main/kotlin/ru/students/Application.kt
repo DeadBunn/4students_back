f34c639e2f34681d0725a3bd9185e35af54d6cfa
Dmitriy Mikhaylov <greatbox04@mail.ru>
2023-11-04 18:37:51 +0300

    bugfix: allow header ContentType

--- src/main/kotlin/ru/students/Application.kt
+++ src/main/kotlin/ru/students/Application.kt
@@ -1,9 +1,11 @@
 package ru.students
 
 import com.typesafe.config.ConfigFactory
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.config.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
 import ru.students.plugins.*
 import ru.vibelab.utils.DatabaseConnection
 import ru.vibelab.utils.PasswordEncryptor
@@ -12,7 +14,24 @@ fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureSecurity()
-    configureHTTP()
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.Authorization)
+        exposeHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.AccessControlAllowHeaders)
+        exposeHeader(HttpHeaders.AccessControlAllowHeaders)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        exposeHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
+        exposeHeader(HttpHeaders.ContentType)
+        allowCredentials = true
+        anyHost()
+        println("Cors has been configured with AccessControlAllowOrigin and ContentType")
+    }
     configureSerialization()
     configureRouting()
     configureFlyway()
