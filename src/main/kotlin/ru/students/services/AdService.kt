3c7fbaf82e3e07a49ffcb128c0a158fcb721503e
Dmitriy Mikhaylov <greatbox04@mail.ru>
2023-12-03 14:14:27 +0300

    fs19: добавил снятие денег с баланса при создании объявления

--- src/main/kotlin/ru/students/services/AdService.kt
+++ src/main/kotlin/ru/students/services/AdService.kt
@@ -1,9 +1,15 @@
 package ru.students.services
 
+import io.ktor.http.*
+import io.ktor.http.content.*
 import ru.students.dtos.AdResponse
+import ru.students.dtos.BaseResponse
 import ru.students.dtos.requests.CreateAdRequest
 import ru.students.mappers.AdMapper
+import ru.students.models.ad.AdType
+import ru.students.models.user.User
 import ru.students.repos.AdRepo
+import ru.students.repos.UserRepo
 
 object AdService {
     fun getAdsResponses(type: String?, tagIds: List<Long>): List<AdResponse> {
@@ -14,12 +20,47 @@ object AdService {
             .map(AdMapper::toResponse)
     }
 
-    fun createAd(request: CreateAdRequest, userId: Long, files: MutableList<Long>): AdResponse {
-        val id = AdRepo.createAd(request, userId)
+    suspend fun createAd(userId: Long, multipart: MultiPartData): BaseResponse<AdResponse> {
+        val fileParts = mutableListOf<PartData.FileItem>()
+        var jsonValue: String? = null
+        multipart.forEachPart { part ->
+            when (part) {
+                is PartData.FileItem -> {
+                    fileParts.add(part)
+                }
 
-        AdRepo.linkTagsToAd(id, request.tags)
-        AdRepo.linkFilesToAd(id, files)
+                is PartData.FormItem -> {
+                    jsonValue = part.value
+                    part.dispose()
+                }
 
-        return AdMapper.toResponse(AdRepo.findAdById(id)!!)
+                else -> {
+                }
+            }
+        }
+        val request = GsonParser.parse(CreateAdRequest::class.java, jsonValue!!)
+
+        val user: User = UserRepo.findUserById(userId)!!
+
+        if (request.type == AdType.ORDER && user.balance < request.price) {
+            return BaseResponse(code = HttpStatusCode.BadRequest, message = "На балансе недостаточно средств")
+        }
+
+        val fileIds = mutableListOf<Long>()
+        fileParts.forEach { filePart ->
+            val fileId = FileService.saveFile(filePart)
+            fileIds.add(fileId)
+            filePart.dispose()
+        }
+
+        return BaseResponse(
+            data = AdMapper.toResponse(
+                AdRepo.createAd(
+                    request = request,
+                    userId = userId,
+                    fileIds = fileIds
+                )
+            )
+        )
     }
 }
